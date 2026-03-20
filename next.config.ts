b89c9884e479a6fcc6f07d26cd9c4dfa96d6a091
関谷虹 <kosekiya@NijiMBP14.local>
2026-03-21 01:02:23 +0900

    next.config修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import { withKeystaticConfig } from "@keystatic/next";
-import keystatic from "./keystatic.config";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
 
-export default withKeystaticConfig(nextConfig, keystatic);
+export default nextConfig;
