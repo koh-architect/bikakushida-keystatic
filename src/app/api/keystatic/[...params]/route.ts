77281e4c4fee4f331c061fb6d3b0ed45fdbafef3
関谷虹 <kosekiya@NijiMBP14.local>
2026-03-21 20:32:06 +0900

    fix: force nodejs runtime for keystatic api

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -1,4 +1,5 @@
 import { makeRouteHandler } from "@keystatic/next/route-handler";
 import config from "../../../../../keystatic.config";
+export const runtime = "nodejs";
 
 export const { GET, POST } = makeRouteHandler({ config });
